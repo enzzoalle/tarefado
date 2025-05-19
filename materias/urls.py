bbc0454496756ad85294ede5e0b800caafd5a744
enzzoalle <enzoedu445@gmail.com>
2025-05-19 12:30:37 -0300

    feat: adicionar e remover matéria

--- materias/urls.py
+++ materias/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.materias, name='materias' ),
     path('<int:materia_id>/', views.materia, name='materia' ),
-
+    path('<int:materia_id>/excluir', views.excluir_materia, name='excluir_materia'),
+    path('nova_materia/', views.nova_materia, name='nova_materia'),
 ]
--- materias/views.py
+++ materias/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from app.models import Materia
 from django.contrib.auth.decorators import login_required
+from app.forms import MateriaForm
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
-@login_required # exige login do usuário para acessar esta aba, no caso as materias
+@login_required
 def materia(request, materia_id):
     """Página de matéria única"""
     materia = Materia.objects.get(id = materia_id)
@@ -11,13 +14,44 @@ def materia(request, materia_id):
     if materia.user != request.user:
         return render(request, 'app/erro_404.html', status=404)
     
-    comentarios = materia.comentario_set.order_by('-date_added') # o "-" é para começar da ordem inversa
+    comentarios = materia.comentario_set.order_by('-date_added')
     context = {'materia': materia, 'comentarios': comentarios}
     return render(request, 'materias/materia.html', context)
 
-@login_required # exige login do usuário para acessar esta aba, no caso as materias
+@login_required
 def materias(request):
     """Página que lista todas as matérias"""
     materias = Materia.objects.filter(user=request.user).order_by('date_added')
     context = {'materias': materias}
     return render(request, 'materias/materias.html', context)
+
+@login_required
+def nova_materia(request):
+    """Para adicionar uma nova matéria"""
+    if request.method != 'POST':
+        # nenhuma matéria submetida, cria um formulário em branco
+        form = MateriaForm()
+    else: 
+        form = MateriaForm(request.POST)
+        if form.is_valid():
+            nova_materia = form.save(commit=False)
+            nova_materia.user = request.user
+            nova_materia.save()
+            return HttpResponseRedirect(reverse('materias'))
+        
+    context = {'form':form}
+    return render(request, 'materias/nova_materia.html', context)
+
+@login_required
+def excluir_materia(request, materia_id):
+    materia = Materia.objects.get(id=materia_id)
+
+    # garante que a matéria pertença ao usuário atual
+    if materia.user != request.user:
+        return render(request, 'app/erro_404.html', status=404)
+    if request.method == 'POST':
+        materia.delete()
+        return HttpResponseRedirect(reverse('materias'))
+    
+    context = {'materia':materia}
+    return render (request, 'materias/excluir_matéria.html', context)
