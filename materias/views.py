2e09a1d5a5a1e256a47bf5be69eb816659efb5ca
enzzoalle <enzoedu445@gmail.com>
2025-06-15 22:01:40 -0300

    refactor: views de materias e tarefas

--- materias/views.py
+++ materias/views.py
@@ -1,101 +1,80 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from app.models import Materia, Comentario
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from app.forms import MateriaForm, ComentarioForm
-from django.urls import reverse
-from django.http import HttpResponseRedirect
 
-@login_required
+@login_required(login_url='login_view')
 def materia(request, materia_id):
-    """Página de matéria única"""
-    materia = Materia.objects.get(id = materia_id)
-
-    # garante que a matéria pertença ao usuário atual
-    if materia.user != request.user:
-        return render(request, 'app/erro_404.html', status=404)
-    
+    """Exibe uma matéria específica e seus comentários"""
+    materia = get_object_or_404(Materia, id=materia_id, user=request.user)
     comentarios = materia.comentario_set.order_by('-date_added')
+
     context = {'materia': materia, 'comentarios': comentarios}
     return render(request, 'materias/materia.html', context)
 
-@login_required
+@login_required(login_url='login_view')
 def materias(request):
-    """Página que lista todas as matérias"""
+    """Lista todas as matérias do usuário"""
     materias = Materia.objects.filter(user=request.user).order_by('date_added')
     context = {'materias': materias}
     return render(request, 'materias/materias.html', context)
 
-@login_required
+@login_required(login_url='login_view')
 def nova_materia(request):
-    """Para adicionar uma nova matéria"""
-    if request.method != 'POST':
-        # nenhuma matéria submetida, cria um formulário em branco
-        form = MateriaForm()
-    else: 
-        form = MateriaForm(request.POST)
+    """Cria uma nova matéria"""
+    form = MateriaForm(request.POST or None)
 
-        if form.is_valid():
-            nova_materia = form.save(commit=False)
-            nova_materia.user = request.user
-            nova_materia.save()
-            return HttpResponseRedirect(reverse('materias'))
-        
-    context = {'form':form}
+    if request.method == 'POST' and form.is_valid():
+        nova = form.save(commit=False)
+        nova.user = request.user
+        nova.save()
+        return redirect('materias')
+
+    context = {'form': form}
     return render(request, 'materias/nova_materia.html', context)
 
-@login_required
+@login_required(login_url='login_view')
 @require_POST
 def excluir_materia(request, materia_id):
+    """Exclui uma matéria"""
     materia = get_object_or_404(Materia, id=materia_id, user=request.user)
     materia.delete()
-    return HttpResponseRedirect(reverse('materias'))
+    return redirect('materias')
 
-@login_required
+@login_required(login_url='login_view')
 def novo_comentario(request, materia_id):
-    materia = Materia.objects.get(id=materia_id)
+    """Adiciona um novo comentário à matéria"""
+    materia = get_object_or_404(Materia, id=materia_id, user=request.user)
+    form = ComentarioForm(request.POST or None)
 
-    if materia.user != request.user:
-        return render(request, 'app/erro_404.html', status=404)
-    
-    if request.method != 'POST':
-        form = ComentarioForm()
-    else:
-        form = ComentarioForm(data=request.POST)
+    if request.method == 'POST' and form.is_valid():
+        comentario = form.save(commit=False)
+        comentario.materia = materia
+        comentario.save()
+        return redirect('materia', materia_id=materia.id)
 
-        if form.is_valid():
-            novo_comentario = form.save(commit=False)
-            novo_comentario.materia = materia
-            novo_comentario.save()
-            return HttpResponseRedirect(reverse('materia', args=[materia_id]))
-        
-    context = {'materia': materia, 'form':form}
+    context = {'materia': materia, 'form': form}
     return render(request, 'materias/novo_comentario.html', context)
 
-@login_required
+@login_required(login_url='login_view')
 def editar_comentario(request, comentario_id):
-    comentario = Comentario.objects.get(id=comentario_id)
-    materia = comentario.materia
+    """Edita um comentário existente"""
+    comentario = get_object_or_404(Comentario, id=comentario_id, materia__user=request.user)
+    form = ComentarioForm(request.POST or None, instance=comentario)
 
-    if materia.user != request.user:
-        return render(request, 'app/erro_404.html', status=404)
-    
-    if request.method != 'POST':
-        form = ComentarioForm(instance=comentario)
-    else:
-        form = ComentarioForm(instance=comentario, data=request.POST)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('materia', materia_id=comentario.materia.id)
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('materia', args=[materia.id]))
-        
-    context = {'comentario': comentario, 'materia': materia, 'form':form}
+    context = {'comentario': comentario, 'materia': comentario.materia,'form': form}
     return render(request, 'materias/editar_comentario.html', context)
 
-@login_required
+@login_required(login_url='login_view')
 @require_POST
 def excluir_comentario(request, comentario_id):
+    """Exclui um comentário"""
     comentario = get_object_or_404(Comentario, id=comentario_id, materia__user=request.user)
     materia_id = comentario.materia.id
     comentario.delete()
-    return HttpResponseRedirect(reverse('materia', args=[materia_id]))
+    return redirect('materia', materia_id=materia_id)
